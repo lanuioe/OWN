ee1efd2658635f82c6df594ae8d0cacae56a125b
lanuioe <lanuioe@gmail.com>
2024-10-01 17:23:10 +0900

    이미지 경로 변경
    - assets 폴더 변경으로 인한 이미지 경로 변경

--- src/component/RandomBg.jsx
+++ src/component/RandomBg.jsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
 import getRandomImage from "../util/getRandomImage";
+import img1 from "../assets/brandStory/mainHome_bg1.webp";
+import img2 from "../assets/brandStory/mainHome_bg2.webp";
+import img3 from "../assets/brandStory/mainHome_bg3.webp";
+import img4 from "../assets/brandStory/mainHome_bg4.webp";
+import img5 from "../assets/brandStory/mainHome_bg5.webp";
+import img6 from "../assets/brandStory/mainHome_bg6.webp";
+import img7 from "../assets/brandStory/mainHome_bg7.webp";
+import img8 from "../assets/brandStory/mainHome_bg8.webp";
 
 const Background = styled.div`
   position: absolute;
@@ -15,14 +23,14 @@ const Background = styled.div`
 
 const RandomBg = ({ setTextColor }) => {
   const images = [
-    { url: "/assets/brandStory/mainHome_bg1.webp", textColor: "#FFFFFF" },
-    { url: "/assets/brandStory/mainHome_bg2.webp", textColor: "#FFFFFF" },
-    { url: "/assets/brandStory/mainHome_bg3.webp", textColor: "#F4F4F4" },
-    { url: "/assets/brandStory/mainHome_bg4.webp", textColor: "#FD4525" },
-    { url: "/assets/brandStory/mainHome_bg5.webp", textColor: "#F4F4F4" },
-    { url: "/assets/brandStory/mainHome_bg6.webp", textColor: "#FD4525" },
-    { url: "/assets/brandStory/mainHome_bg7.webp", textColor: "#FD4525" },
-    { url: "/assets/brandStory/mainHome_bg8.webp", textColor: "#F4F4F4" },
+    { url: img1, textColor: "#FFFFFF" },
+    { url: img2, textColor: "#FFFFFF" },
+    { url: img3, textColor: "#F4F4F4" },
+    { url: img4, textColor: "#FD4525" },
+    { url: img5, textColor: "#F4F4F4" },
+    { url: img6, textColor: "#FD4525" },
+    { url: img7, textColor: "#FD4525" },
+    { url: img8, textColor: "#F4F4F4" },
   ];
 
   const [backgroundImage, setBackgroundImage] = useState(images[0].url);
@@ -31,6 +39,7 @@ const RandomBg = ({ setTextColor }) => {
     const { url, textColor } = getRandomImage(images);
     setBackgroundImage(url);
     setTextColor(textColor);
+    console.log("Image URL:", url);
   }, [setTextColor]);
 
   return <Background $backgroundImage={backgroundImage} />;
