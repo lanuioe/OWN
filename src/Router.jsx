2e19fc59888be60b77b1d08e1faf86e8b7c0147c
g1azed <victoria1230120@gmail.com>
2024-10-26 03:10:33 +0900

    appservice 마크업

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import MainHome from './component/MainHome'
 import IntroBranding from './brandingIntro/IntroBranding'
+import AppService from './appservice/AppService'
 
 const Router = () => {
     return (
@@ -9,7 +10,7 @@ const Router = () => {
             <Routes>
                 <Route path='/' element={<MainHome />} />
                 <Route path='/introbranding' element={<IntroBranding />} />
-
+                <Route path='/appservice' element ={<AppService />} />
             </Routes>
         </BrowserRouter>
     );
