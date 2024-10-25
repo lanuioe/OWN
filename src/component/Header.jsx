d00530a69cc3ae1d5d49407a3b65aee95550df62
lanuioe <lanuioe@gmail.com>
2024-10-25 23:36:18 +0900

    경로별 헤더 position 조건 처리 및 Link 연결 - 풀페이지와 헤더의 fixed가 충돌하는 문제 발생 - 루트 경로에서는 헤더에 absolute, 나머지는 fixed로 조건 처리 - px 단위 vw로 변경 - 로고 및 내비게이션 Link 연결

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,54 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
-import { colors } from '../styles'
-import logo from '../assets/logo/own_logo.svg'
-import SoundBtn from './SoundButton'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
+import { colors } from "../styles";
+import vwCalc from "../util/vwCalc";
+import ImgBox from "./ImgBox";
+import SoundBtn from "./SoundButton";
+import logo from "../assets/logo/own_logo.svg";
+
+const MAIN_ORANGE = colors.mainOrange;
 
 const HeaderWrap = styled.header`
-    width: 100vw;
-    height: 92px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    position: fixed;
-    top: 0;
-`
+  display: flex;
+  position: ${(props) => (props.isRoot ? "absolute" : "fixed")};
+  padding: ${vwCalc(37)} ${vwCalc(80)} 0;
+  top: 0;
+  width: 100vw;
+  height: ${vwCalc(92)};
+  justify-content: space-between;
+  align-items: center;
+  z-index: 10;
+`;
 const ContentListWrap = styled.div`
-    border: 1px solid ${colors.mainOrange};
-    padding: 10px 0;
-    border-radius: 45px;
-`
+  padding: ${vwCalc(12)} 0;
+  width: ${vwCalc(792)};
+  border: ${vwCalc(1)} solid ${MAIN_ORANGE};
+  border-radius: ${vwCalc(45)};
+`;
 const HeaderContentList = styled.ol`
-    display: flex; 
-    justify-content: space-around;
-    align-items: center;
-    padding-right: 56px;
-    padding-left: 56px;
-    gap: 60px;    
-    li{
-        font-size: 1.25vw;
-        color: #FD4525;
-    }
-    li:hover{
-        font-weight: 600;
-    }
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0 ${vwCalc(56)};
+  gap: ${vwCalc(60)};
+
+  li {
+    font-size: 1.25vw;
+    color: ${MAIN_ORANGE};
+  }
+  li:hover {
+    font-weight: 600;
+  }
+`;
 
 const Header = () => {
-    return (
-        <HeaderWrap>
-            <div> <img src={logo} /></div>
-            <ContentListWrap> 
-                <HeaderContentList>
-                    <li> ABOUT </li>
-                    <li> BRANDING </li>
-                    <li> SERVICE </li>
-                    <li> ANALOG </li>
-                </HeaderContentList>
-            </ContentListWrap>
-            <div> <SoundBtn /> </div>
-        </HeaderWrap>
-    );
+  const location = useLocation();
+  const [isRoot, setIsRoot] = useState(
+    location.pathname === "/" || location.pathname === "/#"
+  );
+
+  useEffect(() => {
+    setIsRoot(location.pathname === "/" || location.pathname.includes("/#"));
+
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <HeaderWrap isRoot={isRoot}>
+      <Link to="/">
+        <ImgBox src={logo} alt="OWN 로고" width={110} height={33} />
+      </Link>
+      <ContentListWrap>
+        <HeaderContentList>
+          <li>
+            <Link to="/introbranding">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/">BRANDING</Link>
+          </li>
+          <li>
+            <Link to="/">SERVICE</Link>
+          </li>
+          <li>
+            <Link to="/">ANALOG</Link>
+          </li>
+        </HeaderContentList>
+      </ContentListWrap>
+      <SoundBtn />
+    </HeaderWrap>
+  );
 };
 
 export default Header;
