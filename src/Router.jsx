efe7c2f6a83ec11940d17eb55789783f07220398
lanuioe <lanuioe@gmail.com>
2024-10-25 15:08:33 +0900

    [환경설정] 브랜드스토리 경로 설정 - 기본 경로를 MainHome에서 BrandStory로 변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
-import MainHome from './component/MainHome'
-import IntroBranding from './brandingIntro/IntroBranding'
-import CanvasTest from './canvastest/CanvasTest';
+import React from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import BrandStory from "./component/BrandStory";
+import IntroBranding from "./brandingIntro/IntroBranding";
+import CanvasTest from "./canvastest/CanvasTest";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<MainHome />} />
-                <Route path='/introbranding' element={<IntroBranding />} />
-                <Route path='/canvas' element={<CanvasTest />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BrandStory />} />
+        <Route path="/introbranding" element={<IntroBranding />} />
+        <Route path="/canvas" element={<CanvasTest />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
