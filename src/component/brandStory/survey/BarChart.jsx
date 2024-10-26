da7b766e89d1ebb475dabfa4f86e51dba91c69ad
lanuioe <lanuioe@gmail.com>
2024-10-26 18:28:26 +0900

    [기능] 막대그래프 증가 애니메이션

---
+++ src/component/brandStory/survey/BarChart.jsx
@@ -0,0 +1,28 @@
+import styled, { keyframes, css } from "styled-components";
+import vwCalc from "../../../util/vwCalc";
+
+const increaseGraph = (targetHeight) => keyframes`
+  from { height: 0; }
+  to { height: ${targetHeight}; }
+`;
+
+const BarChart = styled.li`
+  display: flex;
+  flex-direction: column;
+  width: ${vwCalc(215)};
+  height: ${(props) => vwCalc(props.height)};
+  padding-bottom: ${vwCalc(28)};
+  background-color: ${(props) => props.color};
+  justify-content: flex-end;
+  font-family: Pretendard;
+  text-align: center;
+  text-transform: uppercase;
+
+  ${({ targetHeight, isVisible }) =>
+    isVisible &&
+    css`
+      animation: ${increaseGraph(targetHeight)} 2s ease-out forwards;
+    `}
+`;
+
+export default BarChart;
