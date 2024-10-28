22ae75991cb0c2a9d89e18cc75c2bcc9c119b362
lanuioe <lanuioe@gmail.com>
2024-10-28 18:05:29 +0900

    [기능] 브랜드스토리 모션 추가

--- src/component/brandStory/Overflow3.jsx
+++ src/component/brandStory/Overflow3.jsx
@@ -1,9 +1,20 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import vwCalc from "../../util/vwCalc";
 import { BasePoppins100 } from "../style/BasePoppins";
 import { BasePretendard20 } from "../style/BasePretendard";
 import ImgBox from "../ImgBox";
 import breakCircle from "../../assets/brandStory/break_circle.gif";
+import { useState } from "react";
+import useIntersectionObserverRef from "../../hook/useIntersectionObserverRef";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 const Poppins100 = styled(BasePoppins100)`
   position: relative;
@@ -24,11 +35,24 @@ const Pretendard20 = styled(BasePretendard20)`
 const CircleImgBox = styled(ImgBox)`
   position: absolute;
   top: 0;
+  opacity: 0;
+  animation: ${({ isVisible }) => isVisible && fadeIn} 0.5s ease-in forwards;
 `;
 
 const Overflow3 = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleIntersection = () => {
+    setIsVisible(true);
+  };
+
+  const sectionRef = useIntersectionObserverRef({
+    callback: handleIntersection,
+    options: { threshold: 0.2 },
+  });
+
   return (
-    <>
+    <section ref={sectionRef}>
       <Poppins100>
         in a busy life, <br />
         people are forgetting <br />
@@ -38,13 +62,16 @@ const Overflow3 = () => {
       <Pretendard20>
         사람들은 바쁘게 흘러가는 현대사회로 가장 필요한 무언가를 잊어갔습니다.
       </Pretendard20>
-      <CircleImgBox
-        src={breakCircle}
-        alt="원 깨지는 모션"
-        width={1920}
-        height={1080}
-      />
-    </>
+      {isVisible && (
+        <CircleImgBox
+          src={breakCircle}
+          alt="원 깨지는 모션"
+          width={1920}
+          height={1080}
+          isVisible={isVisible}
+        />
+      )}
+    </section>
   );
 };
 
